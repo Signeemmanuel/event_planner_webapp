5e2dd02c9430e89c49260d1e50d533a5e3b0af47
Signeemmanuel <signeemmanuel28@gmail.com>
2023-05-23 04:43:21 +0100

    Add models for event, agender, budget, guest, report, RSVP and buget

--- Backend/event_planner/user_app/models.py
+++ Backend/event_planner/user_app/models.py
@@ -7,3 +7,57 @@ class User(AbstractUser):
     contact = models.IntegerField(blank=True, null=True)
     
     REQUIRED_FIELDS = ['email', 'first_name', 'last_name']
+    
+
+class Budget(models.Model):
+    item_name = models.CharField(max_length=255, blank=False, null=False)
+    item_cost= models.IntegerField(default=0)
+    item_paid = models.BooleanField(default=False)
+
+    def __str__(self):
+        return (self.item_name )
+
+
+class Guest(models.Model):
+    email = models.EmailField(primary_key=True, max_length=255, unique = True)
+    first_name = models.CharField(max_length=255, blank=False, null=False)
+    last_name = models.CharField(max_length=255, blank=False, null=False)
+    contact = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+    
+    
+class Report(models.Model):
+    attendance = models.IntegerField(default=0)
+    total_cost = models.IntegerField(default=0)
+    summary = models.TextField()
+    
+
+class Agender(models.Model):
+    activity_name = models.CharField(max_length=255, blank=False, null=False)
+    time = models.TimeField()
+    
+    def __str__(self):
+        return self.activity_name
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=255, blank=False, null=False)
+    date = models.DateField(blank=False, null=False)
+    time = models.TimeField(blank=False, null=False)
+    location = models.CharField(max_length=255,blank=False, null=False)
+    description = models.TextField()
+    event_type = models.CharField(max_length=255, blank=False, null=False)
+    user_name = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, null=False)
+    budget = models.ForeignKey(Budget, on_delete=models.SET_NULL, blank=False, null=True)
+    report_id = models.ForeignKey(Report, on_delete=models.SET_NULL, blank=False, null=True)
+    agender_id = models.ForeignKey(Agender, on_delete=models.SET_NULL, blank=False, null=True)
+    
+    def __str__(self):
+        return self.name
+
+
+class RSVP(models.Model):
+    event_id = models.ForeignKey(Event, on_delete=models.CASCADE, blank=False, null=False)
+    email = models.ForeignKey(Guest, on_delete=models.CASCADE, blank=False, null=False)
